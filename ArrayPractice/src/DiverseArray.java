27193587b1368efb596885c3d83ce539ba79bc39
BoatNasa25 <Diegovera2511@gmail.com>
2021-12-23 09:25:57 -0600

    Started project

--- ArrayPractice/src/DiverseArray.java
+++ ArrayPractice/src/DiverseArray.java
@@ -7,4 +7,37 @@ public class DiverseArray {
 
         // 3. Complete three static methods arraySum, rowSums, and isDiverse.
     }
+
+    // multiplesOf
+    public static int[] multipliesOf(int num, int count) {
+        int Marray[] = new int[count];
+        // offset by 1
+        for (int i = 1; i < Marray.length; i++) {
+            Marray[i - 1] = (i * num);
+        }
+        return Marray;
+
+    }
+
+    public static int arraySum(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+
+    // RowSums
+    public static int[] rowSums(int arr2D[][]) {
+        int Larray[] = new int[arr2D.length];
+        for (int i = 0; i < Larray.length; i++) {
+            for (int j = 0; j < Larray.length; j++) {
+
+            }
+        }
+
+        return Larray;
+    }
+
 }
